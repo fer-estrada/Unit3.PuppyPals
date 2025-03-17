32520d981370b04952d77ef7621609d60bc548ab
Fernando <fernandodestrada@gmail.com>
2025-03-17 00:17:32 -0400

    workshop block 24

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
+import { puppyList } from './data'
 import './App.css'
 
 function App() {
   
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  console.log(setPuppies);
+
+  const featPup = puppies.find((pup) => pup.id === featPupId)
 
   return (
     <>
-      <div>
+      <div className='App'>
+        { featPupId && (
+          <div className='featured'>
+            <h2>{featPup.name}</h2>
+            <u>
+              <li>Age: {featPup.age}</li>
+              <li>E-mail: {featPup.email}</li>
+            </u>
+          </div>
+        )} 
         
+        {
+          puppies.map((puppy) => {
+            return <p onClick={() => { setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
     </>
   )
